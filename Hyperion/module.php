8770162447cfdbdf12646490baaa92227355e4ce
Micha87 <wessi87@t-online.de>
2025-01-16 21:34:43 +0100

    Update module.php

--- Hyperion/module.php
+++ Hyperion/module.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
 
         // Konfigurationsparameter definieren
         $this->RegisterPropertyString('IPAddress', '');
-	$this->RegisterPropertyInteger('Port', '');
+	$this->RegisterPropertyString('Port', '');
         $this->RegisterPropertyString('Token', '');
     }
 		
